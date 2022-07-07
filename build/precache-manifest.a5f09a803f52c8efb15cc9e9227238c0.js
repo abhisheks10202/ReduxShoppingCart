34424de2aaa0e273223e53a83cf832e07f82ded2
abhisheks10202@gmail.com <abhisheks10202@gmail.com>
2022-07-07 11:02:27 +0530

    redux

---
+++ build/precache-manifest.a5f09a803f52c8efb15cc9e9227238c0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d779538ad55b826357339a6297fc76ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0323d4fc9d7e337ed769",
+    "url": "/static/css/main.d1087339.chunk.css"
+  },
+  {
+    "revision": "80585d3d8dbf48d0407d",
+    "url": "/static/js/2.a1727efb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a1727efb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0323d4fc9d7e337ed769",
+    "url": "/static/js/main.91ae31e2.chunk.js"
+  },
+  {
+    "revision": "b10d08823a4df677ba67",
+    "url": "/static/js/runtime-main.de931e19.js"
+  },
+  {
+    "revision": "028978f40ca009a385c6fb19486bdc53",
+    "url": "/static/media/book.028978f4.jpg"
+  },
+  {
+    "revision": "67a487675e594f3b274f76d190aa011f",
+    "url": "/static/media/smartphone.67a48767.jpg"
+  },
+  {
+    "revision": "4a0dbe85527b88bfcea004afbf6c9fb8",
+    "url": "/static/media/speaker.4a0dbe85.jpg"
+  }
+]);
